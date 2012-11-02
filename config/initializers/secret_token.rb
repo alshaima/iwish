634faa8f190ef95952c42ae2ad39a2dc3af24543
Alshaimaa <engalshaymaa@yahoo.com>
2012-11-02 07:05:28 +0200

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iwish::Application.config.secret_token = '9389308d734c2ca79cbb7477e454c8fa5bbf9063258e511ddc4cd396d0dfb3a6ba50932366349a102f65158ff6180b333f16036be142be5ebc5b024c9a7a3b05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Iwish::Application.config.session_store :cookie_store, key: '_iwish_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Iwish::Application.config.session_store :active_record_store
